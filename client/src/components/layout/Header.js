14bafaa91248e48949bb4df48c0d2743d9153297
Will <wesskuff@gmail.com>
2020-10-02 16:29:01 -0400

    LinkContainer in Header

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import {Nav, Navbar, Container} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
 
 const Header = () => {
 	return (
 		<header>
 			<Navbar bg='dark' variant='dark' collapseOnSelect expand='lg' className='py-3'>
 				<Container>
-					<Navbar.Brand style={{fontSize: '1.65rem'}} href='/'>
-						E.E.E.E.
-					</Navbar.Brand>
+					<LinkContainer to='/'>
+						<Navbar.Brand style={{fontSize: '1.65rem'}}>E.E.E.E.</Navbar.Brand>
+					</LinkContainer>
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
 					<Navbar.Collapse id='basic-navbar-nav'>
 						<Nav className='ml-auto'>
-							<Nav.Link href='/cart' style={{fontSize: '1rem', marginRight: '.65rem'}}>
-								<i className='fas fa-shopping-cart mr-1'></i> Cart
-							</Nav.Link>
-							<Nav.Link href='/login' style={{fontSize: '1rem'}}>
-								Sign In
-							</Nav.Link>
+							<LinkContainer to='/cart'>
+								<Nav.Link style={{fontSize: '1rem', marginRight: '.65rem'}}>
+									<i className='fas fa-shopping-cart mr-1'></i> Cart
+								</Nav.Link>
+							</LinkContainer>
+							<LinkContainer to='/login'>
+								<Nav.Link style={{fontSize: '1rem'}}>Sign In</Nav.Link>
+							</LinkContainer>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
