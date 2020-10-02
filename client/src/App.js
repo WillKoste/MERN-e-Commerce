cab59e6076c80d73c7251a629688c410e0f4e0c4
Will <wesskuff@gmail.com>
2020-10-01 23:27:25 -0400

    Sync

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+import Navbar from './components/layout/Navbar';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+
+const App = () => {
 	return (
 		<div className='App'>
+			<Header />
+			<Navbar />
 			<h1>Ding Dong</h1>
 			<h2>Poo Pong</h2>
+			<Footer />
 		</div>
 	);
-}
+};
 
 export default App;
---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => {
+	return <footer>Da Foot</footer>;
+};
+
+export default Footer;
