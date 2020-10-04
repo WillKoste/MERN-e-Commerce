0d351d178f40f76b4b018dbd7c127e17c6620b32
Will Koste <willkoste@gmail.com>
2020-10-04 12:01:23 -0400

    Products CRUD

---
+++ routes/products.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../config/db');
+const {check, validationResult} = require('express-validator');
+
+router.get('/', async (req, res) => {
+	try {
+		const products = await pool.query(`SELECT * FROM products`);
+
+		res.json({success: true, count: products.rowCount, products: products.rows});
+	} catch (err) {
+		console.error(err);
+		res.send('Server Error');
+	}
+});
+
+router.get('/:id', async (req, res) => {
+	try {
+		const product = await pool.query(`SELECT * FROM products WHERE id = ${req.params.id}`);
+
+		if (product.rows.length === 0) {
+			return res.status(404).json({success: false, msg: `No user found with the id of ${req.params.id}`});
+		}
+
+		res.json({success: true, product: product.rows[0]});
+	} catch (err) {
+		console.error(err);
+		res.send('Server Error');
+	}
+});
+
+router.post('/', [check('name', 'Product name is required').not().isEmpty(), check('price', 'Product price is required').not().isEmpty(), check('countinstock', 'Must include the total inventory of the product being created').not().isEmpty()], async (req, res) => {
+	const errors = validationResult(req);
+
+	if (!errors.isEmpty()) {
+		return res.status(400).json({success: false, errors: errors.array()});
+	}
+
+	const {name, image, description, brand, category, price, countinstock, rating, numreviews} = req.body;
+
+	try {
+		let product = await pool.query(`INSERT INTO products (name, image, description, brand, category, price, countinstock, rating, numreviews) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`, [name, image, description, brand, category, price, countinstock, rating, numreviews]);
+
+		res.status(201).json({success: true, msg: 'Product has been added!'});
+	} catch (err) {
+		console.error(err);
+		res.status(400).json({success: false, msg: 'Bad request, please try again'});
+	}
+});
+
+router.put('/:id', [check('name', 'Product name is required').not().isEmpty(), check('price', 'Product price is required').not().isEmpty(), check('countinstock', 'Must include the total inventory of the product being created').not().isEmpty()], async (req, res) => {
+	const errors = validationResult(req);
+
+	if (!errors.isEmpty()) {
+		return res.status(400).json({success: false, errors: errors.array()});
+	}
+
+	const {name, image, description, brand, category, price, countinstock, rating, numreviews} = req.body;
+
+	try {
+		const product = await pool.query(`SELECT * FROM products WHERE id = ${req.params.id}`);
+
+		if (product.rows.length === 0) {
+			return res.status(404).json({success: false, msg: `No user found with the id of ${req.params.id}`});
+		}
+
+		let updateProduct = await pool.query(`UPDATE products SET name = $1, image = $2, description = $3, brand = $4, category = $5, price = $6, countinstock = $7, rating = $8, numreviews = $9 WHERE id = ${req.params.id}`, [name, image, description, brand, category, price, countinstock, rating, numreviews]);
+
+		res.status(201).json({success: true, msg: 'Product has been updated', product: await pool.query(`SELECT * FROM products WHERE id = ${req.params.id}`)});
+	} catch (err) {
+		console.error(err);
+		res.status(400).json({success: false, msg: 'Bad request, please try again'});
+	}
+});
+
+router.delete('/:id', async (req, res) => {
+	try {
+		const product = await pool.query(`SELECT * FROM products WHERE id = ${req.params.id}`);
+
+		if (product.rows.length === 0) {
+			return res.status(404).json({success: false, msg: `No user found with the id of ${req.params.id}`});
+		}
+
+		const deleteProduct = await pool.query(`DELETE FROM products WHERE id = ${req.params.id}`);
+
+		res.json({success: true, msg: `Product ${req.params.id} has been deleted!`});
+	} catch (err) {
+		console.error(err);
+		res.status(400).json({success: false, msg: 'Bad request, please try again'});
+	}
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config({path: './config/config.env'});
+const cors = require('cors');
+const morgan = require('morgan');
 const colors = require('colors');
+const helmet = require('helmet');
 
 const app = express();
+
+app.use(cors());
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use(helmet());
+
+app.use('/api/product', require('./routes/products'));
 
 const port = process.env.PORT || 5000;
 const mode = process.env.NODE_ENV || 'Default';
