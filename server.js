98a6658694aaa0a18baf4281b82b67875b01d444
Will Koste <willkoste@gmail.com>
2021-02-01 10:59:06 -0500

    Static path

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config({path: './config/config.env'});
@@ -23,6 +24,14 @@ app.use('/api/product', require('./routes/products'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/reviews', require('./routes/reviews'));
 app.use('/api/orders', require('./routes/orders'));
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const port = process.env.PORT || 5000;
 const mode = process.env.NODE_ENV || 'Default';
